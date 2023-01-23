c78450096030c4ba96693bef40285c8ca06becb4
cengizhulya <hulyacengiz0@hotmail.com>
2023-01-23 23:50:21 +0300

    Girilen Sayilardan Ciftleri Yazdiran Program

---
+++ GirilenSayilardanCiftleriBulanProgram/GirilenSayilardanCiftleriBulanProgram/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GirilenSayilardanCiftleriBulanProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Döngüler ile kullanıcının girdiği sayıdan 0'a kadar olan cift sayilari ekrana yazdiriyoruz.
+
+            int sayi;
+
+            Console.Write("Bir sayi giriniz:");
+            sayi = int.Parse(Console.ReadLine());
+
+            for (int i = sayi; i >= 0; i--)
+            {
+                if (i %2 ==0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+
+
+
+
+
