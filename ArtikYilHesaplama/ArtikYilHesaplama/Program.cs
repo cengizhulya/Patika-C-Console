f43d98cefc822f7f072875a4a90b8ce0cc39d7ff
cengizhulya <hulyacengiz0@hotmail.com>
2023-01-23 23:01:24 +0300

    Artik Yil Hesaplama Programi

---
+++ ArtikYilHesaplama/ArtikYilHesaplama/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtikYilHesaplama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ***Artik Yil Hesaplama*** \n");
+
+            int yil;
+            double kalan;
+
+            Console.Write("Yıl giriniz:");
+            yil = int.Parse(Console.ReadLine());
+
+            if (yil % 4 == 0)
+            {
+                Console.WriteLine("{0}",yil+" artik yildir");
+            }
+            else if (yil % 400 == 0)
+            {
+                Console.WriteLine("{0}", yil + " artik yildir");
+            }
+            else
+            {
+                Console.WriteLine("{0}", yil + " artik yil degildir");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
