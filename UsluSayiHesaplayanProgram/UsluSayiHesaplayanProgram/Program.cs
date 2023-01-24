a6eec70416a3bb10c7683ad474ed86a0e8b319f3
cengizhulya <hulyacengiz0@hotmail.com>
2023-01-24 22:15:51 +0300

    Uslu Sayi Hesaplayan Program

---
+++ UsluSayiHesaplayanProgram/UsluSayiHesaplayanProgram/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsluSayiHesaplayanProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int taban;
+            int us;
+            int us_;
+
+            Console.Write("Ussu alinacak sayi:");
+            taban = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            Console.Write("Us:");
+            us = int.Parse(Console.ReadLine());
+
+            us_=us;
+
+            int sonuc = 1;
+
+            while (us >= 1)
+            {
+                sonuc = sonuc * taban;
+                us--;
+            }
+
+
+            Console.WriteLine(taban+"'sayisinin "+us_+".kuvveti:"+sonuc);
+
+            Console.ReadLine();
+        }
+    }
+}
