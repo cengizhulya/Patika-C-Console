96fc2cf9b9fec0379ab08115dcd133329d9a5a66
cengizhulya <hulyacengiz0@hotmail.com>
2023-01-24 21:57:50 +0300

    Girilen Sayiya Kadar Olan 4 ve 5'in Kuvvetleri
    Döngüler ile girilen sayıya kadar olan 4 ve 5'in kuvvetlerini ekrana yazdıran programı yazıyoruz.

---
+++ GirilenSayiyaKadar4ve5inKuvvetleri/GirilenSayiyaKadar4ve5inKuvvetleri/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GirilenSayiyaKadar4ve5inKuvvetleri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int taban1 = 4;
+            int taban2 = 5;
+            int sayi;
+            int sonuc1 = 1;
+            int sonuc2 = 1;
+
+            Console.Write("Hangi sayiya kadar 4 ve 5'in kuvvetleri yazilsin:");
+            sayi = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i <=100; i++)
+            {
+                sonuc1 = sonuc1 * taban1;
+                
+                if (sonuc1 > sayi)
+                {
+                    break;
+                }
+
+                Console.WriteLine(sonuc1);
+
+            }
+
+            for (int i = 0; i <= 100; i++)
+            {
+                sonuc2 = sonuc2 * taban2;
+              
+                if (sonuc2 > sayi)
+                {
+                    break;
+                }
+
+                Console.WriteLine(sonuc2);
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
